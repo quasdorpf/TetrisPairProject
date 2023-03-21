9afb721c7238a8ca9943f6f748ddff52c3085a33
damis <damis@10.129.228.95>
2023-03-21 10:02:38 -0400

    Made src

---
+++ TetrisPairProject/src/Block.java
@@ -0,0 +1,7 @@
+import java.awt.*;
+
+public class Block {
+	private Color color;
+	private int[] gridCoords;
+
+}
---
+++ TetrisPairProject/src/Grid.java
@@ -0,0 +1,7 @@
+
+public class Grid {
+	private Tetromino thisTetr;
+	private Tetromino heldTetr;
+	private Tetromino[] nextTetr;
+
+}
