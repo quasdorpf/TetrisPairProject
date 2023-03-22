090e63623c9727e07cc1a3ee0946bec2d127c8bd
mparke2914 <125300129+mparke2914@users.noreply.github.com>
2023-03-22 09:14:23 -0400

    I think I can push now

--- TetrisPairProject/src/Tetromino.java
+++ TetrisPairProject/src/Tetromino.java
@@ -1,4 +1,21 @@
 
 public abstract class Tetromino {
-
+	public static final int LEFT = -1;
+	public static final int RIGHT = 1;
+	private Block[][][] rotations;;
+	private int currRotation = 0; // would be used in rotations[currRotation]
+	public void rotate(int leftOrRight) {
+		if (leftOrRight == -1 || leftOrRight == 1) {
+			currRotation += leftOrRight;
+			if (currRotation > 3) {
+				currRotation -= 4;
+			}
+			if (currRotation < 0) {
+				currRotation += 4;
+			}
+		}
+	}
+	public Block[][] getRotation() {
+		return rotations[currRotation];
+	}
 }
