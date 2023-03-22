820410ee5ae0c3d3ee9cff29631ea6d3f57b787f
mparke2914 <125300129+mparke2914@users.noreply.github.com>
2023-03-22 12:43:48 -0400

    Block update and ITetromino

--- TetrisPairProject/src/Block.java
+++ TetrisPairProject/src/Block.java
@@ -2,9 +2,10 @@ import java.awt.*;
 
 public class Block {
 	private Color color;
-	private boolean empty = false;
-	public Block(Color color) {
+	private boolean empty;
+	public Block(Color color, boolean empty) {
 		this.color = color;
+		this.empty = empty;
 	}
 	public Color getColor() {
 		return color;
--- TetrisPairProject/src/ITetromino.java
+++ TetrisPairProject/src/ITetromino.java
@@ -1,4 +1,30 @@
+import java.awt.Color;
 
 public class ITetromino extends Tetromino {
-	
+	private static final Color color = Color.cyan;
+	public ITetromino() {
+		Block[][][] rotations = new Block[4][4][4];
+		for (int i=0; i<rotations.length; i++) {
+			for (int j=0; j<rotations[0].length; j++) {
+				for (int k=0; k<rotations[0][0].length; k++) {
+					rotations[i][j][k] = new Block(color, true);
+				}
+			}
+		}
+		for (int i=0; i<4; i++) {
+			// 0 - UP
+			rotations[0][i][1].setEmpty(false);
+			
+			// 1 - RIGHT
+			rotations[1][2][i].setEmpty(false);
+			
+			// 2 - DOWN
+			rotations[2][i][2].setEmpty(false);
+			
+			// 3 - LEFT
+			rotations[3][1][i].setEmpty(false);
+		}
+		
+		setRotations(rotations);
+	}
 }
