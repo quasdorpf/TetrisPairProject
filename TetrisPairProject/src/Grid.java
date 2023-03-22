6484921d43bbfa969f67e50fcb6417ba6526ce89
damis <damis@10.129.228.96>
2023-03-22 10:14:35 -0400

    Grid And Tetromino

--- TetrisPairProject/src/Grid.java
+++ TetrisPairProject/src/Grid.java
@@ -3,6 +3,7 @@ public class Grid {
 	private Tetromino currTetr;
 	private Tetromino heldTetr;
 	private Tetromino[] nextTetr;
+	private Block[][] visibleGrid;
 	private Block[][] grid;
 	private int score;
 
@@ -10,6 +11,13 @@ public class Grid {
 		currTetr = tetr;
 	}
 	public void setTetr() {
+		Block[][] tetromino = currTetr.getRotation();
+		for(int i=0;i<tetromino.length;i++){
+			for(int j=0;j<tetromino[0].length;j++){
+				grid[i+currTetr.getY()][i+currTetr.getX()]=tetromino[i][j];
+			}
+		}
+				
 		
 	}
 
--- TetrisPairProject/src/Tetromino.java
+++ TetrisPairProject/src/Tetromino.java
@@ -2,7 +2,8 @@
 public abstract class Tetromino {
 	public static final int LEFT = -1;
 	public static final int RIGHT = 1;
-	private Block[][][] rotations;;
+	private Block[][][] rotations;
+	private int[] position;
 	private int currRotation = 0; // would be used in rotations[currRotation]
 	public void rotate(int leftOrRight) {
 		if (leftOrRight == -1 || leftOrRight == 1) {
@@ -17,6 +18,12 @@ public abstract class Tetromino {
 	}
 	public Block[][] getRotation() {
 		return rotations[currRotation];
+	}
+	public int getX() {
+		return position[1];
+	}
+	public int getY() {
+		return position[0];
 	}
 	public static void main(String[] args) {
 		
