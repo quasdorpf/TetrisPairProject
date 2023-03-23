8754763a25879844db051628b8f8a5608ec56dfb
damis <damis@10.129.228.96>
2023-03-23 19:47:47 -0400

    can hold and set Tetrominos in Grid

--- TetrisPairProject/src/Grid.java
+++ TetrisPairProject/src/Grid.java
@@ -1,8 +1,9 @@
+import java.util.*;
 
 public class Grid {
 	private Tetromino currTetr;
 	private Tetromino heldTetr;
-	private Tetromino[] nextTetr;
+	private ArrayList<Tetromino> nextTetr;
 	private Block[][] visibleGrid;
 	private Block[][] grid;
 	private int score;
@@ -10,8 +11,24 @@ public class Grid {
 	public void dropTetr(Tetromino tetr) {
 		currTetr = tetr;
 	}
+	public void fallTetr(Tetromino tetr) {
+		tetr.shift('D');
+	}
+	public void holdTetr() {
+		if (heldTetr == null) {
+			dropTetr(nextTetr.get(0));
+			nextTetr.remove(0);
+			nextTetr.add(Tetromino.getRandomTetromino());
+		} else {
+			Tetromino tempTetr = currTetr;
+			dropTetr(heldTetr);
+			tempTetr.setPosition(0, 0);
+			heldTetr = tempTetr;
+		}
+	}
 	public void setTetr() {
 		Block[][] tetromino = currTetr.getRotation();
+		ArrayList<Integer> clearRows = new ArrayList<Integer>();
 		for(int i=0;i<tetromino.length;i++){
 			for(int j=0;j<tetromino[0].length;j++){
 				int row = i+currTetr.getY();
@@ -19,9 +36,24 @@ public class Grid {
 				if (!tetromino[i][j].isEmpty())
 					grid[row][col]=tetromino[i][j];
 			}
-			for (int j=0;j<10;j++);
+			boolean canClear = true;
+			for (int j=0;j<10;j++) {
+				canClear&=!tetromino[i][j].isEmpty();
+			}
+			if(canClear) {
+				clearRows.add(i);
+			}
+			clearRows(clearRows);
+			dropTetr(nextTetr.get(0));
+			nextTetr.remove(0);
+			nextTetr.add(Tetromino.getRandomTetromino());
 		}
 		
 	}
-
+	private void clearRows(ArrayList<Integer> rows) {
+		
+	}
+	public Block[][] getGrid(){
+		return grid.clone();
+	}
 }
--- TetrisPairProject/src/Tetromino.java
+++ TetrisPairProject/src/Tetromino.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 public abstract class Tetromino {
 	public static final int LEFT = -1;
@@ -19,8 +20,32 @@ public abstract class Tetromino {
 			}
 		}
 	}
+	public static Tetromino getRandomTetromino() {
+		int rand = new Random().nextInt(7);
+		switch (rand) {
+		case 0:return new OTetromino();
+		case 1:return new ITetromino();
+		case 2:return new ZTetromino();
+		case 3:return new JTetromino();
+		case 4:return new TTetromino();
+		case 5:return new STetromino();
+		case 6:return new LTetromino();
+		default:return new ITetromino();
+		}
+	}
+	public void setPosition(int row,int col) {
+		position = new int[] {row, col};
+	}
 	public Block[][] getRotation() {
 		return rotations[currRotation];
+	}
+	public void shift(char dir) {
+		switch (dir) {
+		case 'D':position[0]-- ;
+		case 'R':position[1]++;
+		case 'L':position[1]--;
+		default:;
+		}
 	}
 	public int getX() {
 		return position[1];
