9f6b830cd35d312939848bc8f567653228104873
mparke2914 <125300129+mparke2914@users.noreply.github.com>
2023-03-27 17:53:22 -0400

    Blocks can be drawn now

--- TetrisPairProject/src/Block.java
+++ TetrisPairProject/src/Block.java
@@ -45,44 +45,52 @@ public class Block {
 			int sideSize = (int)((double)size * 0.1);
 			int midSize = size - (sideSize*2);
 			
-			// Top
-			
-			
-			// Left
-			
-			
 			// Middle
 			g.setColor(color);
 			g.fillRect(x+sideSize, y+sideSize, midSize, midSize);
 			
-			// Right
+			// Top
+			g.setColor(lightColor);
+			int[] xCoordsT = {x, x+size, x+size-sideSize, x+sideSize};
+			int[] yCoordsT = {y, y, y+sideSize, y+sideSize};
+			g.fillPolygon(xCoordsT, yCoordsT, 4);
 			
+			// Left
+			g.setColor(shadedColor);
+			int[] xCoordsL = {x, x+sideSize, x+sideSize, x};
+			int[] yCoordsL = {y, y+sideSize, y+size-sideSize, y+size};
+			g.fillPolygon(xCoordsL, yCoordsL, 4);
+			
+			// Right
+			int[] xCoordsR = {x+size, x+size-sideSize, x+size-sideSize, x+size};
+			int[] yCoordsR = {y, y+sideSize, y+size-sideSize, y+size};
+			g.fillPolygon(xCoordsR, yCoordsR, 4);
 			
 			// Bottom
-			
+			g.setColor(darkColor);
+			int[] xCoordsD = {x, x+sideSize, x+size-sideSize, x+size};
+			int[] yCoordsD = {y+size, y+size-sideSize, y+size-sideSize, y+size};
+			g.fillPolygon(xCoordsD, yCoordsD, 4);
 		}
 	}
 	private void setLightShadedAndDarkColors() {
-		int intensity = 2;
-		int[] baseRGB = getBaseRGBValues(color, intensity);
-		int[] light = {255, 255, 255};
-		int[] shaded = {color.getRed(), color.getGreen(), color.getBlue()};
-		int[] dark = {0, 0, 0};
+		float[] hsb = Color.RGBtoHSB(color.getRed(), color.getGreen(), color.getBlue(), null);
+		float[] lightHSB = new float[3];
+		float[] shadedHSB = new float[3];
+		float[] darkHSB = new float[3];
 		for (int i=0; i<3; i++) {
-			light[i] -= baseRGB[i];
-			shaded[i] -= intensity;
-			dark[i] += baseRGB[i];
+			lightHSB[i] = hsb[i];
+			shadedHSB[i] = hsb[i];
+			darkHSB[i] = hsb[i];
 		}
-		lightColor = new Color(light[0], light[1], light[2]);
-		shadedColor = new Color(shaded[0], shaded[1], shaded[2]);
-		darkColor = new Color(dark[0], dark[1], dark[2]);
-	}
-	private int[] getBaseRGBValues(Color c, int intensity) {
-		int[] rgb = {c.getRed(), c.getGreen(), c.getBlue()};
-		int minVal = Math.min(Math.min(rgb[0], rgb[1]), Math.min(rgb[1], rgb[2]));
-		for (int i=0; i<rgb.length; i++) {
-			rgb[i] = ((rgb[i] / minVal) + 1) * intensity;
-		}
-		return rgb;
+		
+		lightHSB[1] = (float) (hsb[1] * 0.6);
+		lightColor = Color.getHSBColor(lightHSB[0], lightHSB[1], lightHSB[2]);
+		
+		shadedHSB[2] = (float) (hsb[2] * 0.9);
+		shadedColor = Color.getHSBColor(shadedHSB[0], shadedHSB[1], shadedHSB[2]);
+		
+		darkHSB[2] = (float) (hsb[2] * 0.7);
+		darkColor = Color.getHSBColor(darkHSB[0], darkHSB[1], darkHSB[2]);
 	}
 }
