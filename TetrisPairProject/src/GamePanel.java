2439d2aa63d12a71b0777799478d49ee7b7a034a
mparke2914 <125300129+mparke2914@users.noreply.github.com>
2023-04-12 10:19:42 -0400

    Grid outline decoration

--- TetrisPairProject/src/GamePanel.java
+++ TetrisPairProject/src/GamePanel.java
@@ -7,13 +7,14 @@ public class GamePanel extends JPanel {
 			new ZTetromino()
 	};
 	private static final double BLOCK_PERC_SIZE = 0.04;
+	private static final double GRID_PERC_X = 0.32;
+	private static final double GRID_PERC_Y = 0.05;
 	
 	private JFrame screen = RunTetris.screen;
 	private WelcomeScreen welcomeScreen = RunTetris.welcomeScreen;
 	private Leaderboard leaderboard = RunTetris.leaderboard;
 	
-	private int blockSize;
-	private int gridblockSize;
+	private static int blockSize;
 	
 	public GamePanel(int width, int height) {
 		setPreferredSize(new Dimension(width, height));
@@ -38,6 +39,7 @@ public class GamePanel extends JPanel {
 		}
 		*/
 		welcomeScreen.display(g, screen, this);
+		drawGrid(g, RunTetris.getWidthPerc(GRID_PERC_X), RunTetris.getHeightPerc(GRID_PERC_Y), blockSize);
 	}
 	
 	public void drawGridAndBackground(Graphics g) {
@@ -50,15 +52,16 @@ public class GamePanel extends JPanel {
 	
 	private void drawGrid(Graphics g, int x, int y, int blockSize) {
 		Block[][] visGrid = RunTetris.grid.makeVisible();
-		drawGridOutline(g, RunTetris.getWidthPerc(0.05), RunTetris.getHeightPerc(0.05), visGrid[0].length, visGrid.length, blockSize);
+		drawGridOutline(g, x, y, visGrid[0].length, visGrid.length, blockSize);
 		for (int i=0; i<visGrid.length; i++) {
 			for (int j=0; j<visGrid[0].length; j++) {
-				visGrid[i][j].draw(g, RunTetris.getWidthPerc(0.05)+(j*blockSize)+1, RunTetris.getHeightPerc(0.05)+(i*blockSize)+1, blockSize);
+				visGrid[i][j].draw(g, x+(j*blockSize)+1, y+(i*blockSize)+1, blockSize);
 			}
 		}
 	}
 	
 	private void drawGridOutline(Graphics g, int x, int y, int xCount, int yCount, int blockSize) {
+		g.setColor(Color.BLACK);
 		g.drawRect(x, y, (xCount*blockSize)+2, (yCount*blockSize)+2);
 		for (int i=0; i<yCount; i++) {
 			for (int j=0; j<xCount; j++) {
@@ -74,5 +77,42 @@ public class GamePanel extends JPanel {
 		
 		g.setColor(new Color(10, 10, 10)); // Very dark grey is better than absolute black
 		g.fillRect(x+2, y+2, size-3, size-3);
+	}
+	
+	public static void drawLayeredRoundRect(Graphics g, int x, int y, int width, int height, int arcWidth, int arcHeight, 
+			int layers, Color startColor, int colInc) {
+		g.setColor(startColor);
+		for (int i=0; i<layers; i++) {
+			for (int arcI=0; arcI<6; arcI++) {
+				g.drawRoundRect(x+i, y+i, width-(i*2), height-(i*2), arcWidth-arcI, arcHeight-arcI);
+			}
+			Color gCol = g.getColor();
+			int newRed = range(gCol.getRed()+colInc, 0, 255);
+			int newGreen = range(gCol.getGreen()+colInc, 0, 255);
+			int newBlue = range(gCol.getBlue()+colInc, 0, 255);
+			g.setColor(new Color(newRed, newGreen, newBlue));
+		}
+	}
+	
+	private static int range(int num, int min, int max) {
+		if (num < min) {
+			return min;
+		} else if (num > max) {
+			return max;
+		} else {
+			return num;
+		}
+	}
+	
+	public static void drawGridOutlineDecoration(Graphics g) {
+		int x = RunTetris.getWidthPerc(GRID_PERC_X-0.013);
+		int y = RunTetris.getHeightPerc(GRID_PERC_Y-0.017);
+		int width = blockSize*10 + RunTetris.getWidthPerc(0.029);
+		int height = blockSize*20+RunTetris.getHeightPerc(0.039);
+		int arcWidth = RunTetris.getWidthPerc(0.04);
+		int arcHeight = RunTetris.getWidthPerc(0.04);
+		Color color = new Color(10, 10, 10);
+		drawLayeredRoundRect(g, x-5, y-5, width+10, height+10, arcWidth, arcHeight, 5, color, 0);
+		drawLayeredRoundRect(g, x, y, width, height, arcWidth, arcHeight, 20, color, 10);
 	}
 }
--- TetrisPairProject/src/WelcomeScreen.java
+++ TetrisPairProject/src/WelcomeScreen.java
@@ -7,37 +7,13 @@ public class WelcomeScreen {
 	}
 	
 	public void display(Graphics g, JFrame screen, GamePanel gamePanel) {
-		g.drawImage(new ImageIcon("Imgs/tetris-logo.png").getImage(), RunTetris.getWidthPerc(0.3), RunTetris.getHeightPerc(0.2), 
-				RunTetris.getWidthPerc(0.4), RunTetris.getHeightPerc(0.3), gamePanel);
-		drawLayeredRoundRect(g, RunTetris.getWidthPerc(0.1), RunTetris.getHeightPerc(0.1), RunTetris.getWidthPerc(0.6), 
-					RunTetris.getHeightPerc(0.6), RunTetris.getWidthPerc(0.05), RunTetris.getHeightPerc(0.05), 10);
+		//g.drawImage(new ImageIcon("Imgs/tetris-logo.png").getImage(), RunTetris.getWidthPerc(0.3), RunTetris.getHeightPerc(0.2), 
+				//RunTetris.getWidthPerc(0.4), RunTetris.getHeightPerc(0.3), gamePanel);
+		GamePanel.drawGridOutlineDecoration(g);
 		
-		gamePanel.add(playButton);
+		//gamePanel.add(playButton);
 		
 	}
 	
-	private void drawLayeredRoundRect(Graphics g, int x, int y, int width, int height, int arcWidth, int arcHeight, int layers) {
-		g.setColor(Color.BLACK);
-		for (int i=0; i<layers; i++) {
-			for (int arcI=0; arcI<6; arcI++) {
-				g.drawRoundRect(x+i, y+i, width-(i*2), height-(i*2), arcWidth-arcI, arcHeight-arcI);
-			}
-			Color gCol = g.getColor();
-			int colInc = 10;
-			int newRed = gCol.getRed()+colInc;
-			int newGreen = gCol.getGreen()+colInc;
-			int newBlue = gCol.getBlue()+colInc;
-			if (newRed > 255) {
-				newRed = 255;
-			}
-			if (newGreen > 255) {
-				newGreen = 255;
-			}
-			if (newBlue > 255) {
-				newBlue = 255;
-			}
-			g.setColor(new Color(newRed, newGreen, newBlue));
-		}
-	}
 	
 }
